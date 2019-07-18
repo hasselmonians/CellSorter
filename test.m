6c074b8b0280b378e512e1191951722a5b654ec4
Alec Hoyland <alec.hoyland@posteo.net>
2019-07-17 21:43:02 -0400

    Update test script to use wrapper functions

--- test.m
+++ test.m
@@ -2,69 +2,28 @@
 % performs dimensionality-reduction
 % then clusters using k-means and plots the results
 
+% load data
 load fisheriris
-X = meas';
+% meas and species
 
-% perform dimensionality-reduction with umap
-u = umap;
-Y = u.fit(X);
-
-% define options for k-means algorithm
-sset = statset;
-sset.Display = 'iter';
-sset.MaxIter = 100;
-sset.UseParallel = true;
-
-% perform k-means clustering into three classes
-[idx, c, sumd, d] = kmeans(Y, 3, 'Distance', 'sqeuclidean', 'Start', 'plus', 'Replicates', 3, 'EmptyAction', 'error', 'Options', sset);
-
-% visualize the results
-figure; hold on;
+% generate a figure
+figure;
 C = colormaps.linspecer(3);
-plot(Y(idx == 1, 1), Y(idx == 1, 2), 'o', 'Color', C(1, :))
-plot(Y(idx == 2, 1), Y(idx == 2, 2), 'o', 'Color', C(2, :))
-plot(Y(idx == 3, 1), Y(idx == 3, 2), 'o', 'Color', C(3, :))
 
-figlib.pretty('PlotBuffer', 0.2);
+for ii = 3:-1:1
+  subplot(1, 3, ii);
+end
 
-% perform dimensionality-reduction with FIt-SNE
-Y = fast_tsne(X');
+% perform the dimensionality reduction and clustering
+% once for each algorithm
+alg = {'UMAP', 'FIt-SNE', 'PCA'};
 
-% define options for k-means algorithm
-sset = statset;
-sset.Display = 'iter';
-sset.MaxIter = 100;
-sset.UseParallel = true;
-
-% perform k-means clustering into three classes
-[idx, c, sumd, d] = kmeans(Y, 3, 'Distance', 'sqeuclidean', 'Start', 'plus', 'Replicates', 3, 'EmptyAction', 'error', 'Options', sset);
-
-% visualize the results
-figure; hold on;
-C = colormaps.linspecer(3);
-plot(Y(idx == 1, 1), Y(idx == 1, 2), 'o', 'Color', C(1, :))
-plot(Y(idx == 2, 1), Y(idx == 2, 2), 'o', 'Color', C(2, :))
-plot(Y(idx == 3, 1), Y(idx == 3, 2), 'o', 'Color', C(3, :))
-
-figlib.pretty('PlotBuffer', 0.2);
-
-% perform dimensionality-reduction with umap
-Y = pca(X);
-
-% define options for k-means algorithm
-sset = statset;
-sset.Display = 'iter';
-sset.MaxIter = 100;
-sset.UseParallel = true;
-
-% perform k-means clustering into three classes
-[idx, c, sumd, d] = kmeans(Y, 3, 'Distance', 'sqeuclidean', 'Start', 'plus', 'Replicates', 3, 'EmptyAction', 'error', 'Options', sset);
-
-% visualize the results
-figure; hold on;
-C = colormaps.linspecer(3);
-plot(Y(idx == 1, 1), Y(idx == 1, 2), 'o', 'Color', C(1, :))
-plot(Y(idx == 2, 1), Y(idx == 2, 2), 'o', 'Color', C(2, :))
-plot(Y(idx == 3, 1), Y(idx == 3, 2), 'o', 'Color', C(3, :))
+for ii = 1:length(alg)
+  labels = kcluster(dimred(meas, 'Algorithm', alg{ii}));
+  plot(ax(ii), Y(idx == 1, 1), Y(idx == 1, 2), 'o', 'Color', C(1, :))
+  plot(ax(ii), Y(idx == 2, 1), Y(idx == 2, 2), 'o', 'Color', C(2, :))
+  plot(ax(ii), Y(idx == 3, 1), Y(idx == 3, 2), 'o', 'Color', C(3, :))
+  title(ax(ii), alg{ii})
+end
 
 figlib.pretty('PlotBuffer', 0.2);
