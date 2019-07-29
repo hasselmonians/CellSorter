431f2958e717216db8a7afbfec0c0d4e40b80723
Alec Hoyland <alec.hoyland@posteo.net>
2019-07-29 17:36:03 -0400

    Add CellSorter class

---
+++ @CellSorter/CellSorter.m
@@ -0,0 +1,25 @@
+classdef CellSorter
+
+properties
+
+sset      % contains a statset structure
+nClusters % how many clusters do you want?
+nDims     % how many dimensions do you want?
+verbosity = true;
+algorithm = 'umap';
+
+end % properties
+
+properties (SetAccess = Protected)
+
+end % properties setaccess protected
+
+methods
+
+end % methods
+
+methods (Static)
+
+end % static methods
+
+end % classdef
