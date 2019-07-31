d07a46672c387bc6e253205571ba91ee6d5cdbd2
Alec Hoyland <alec.hoyland@posteo.net>
2019-07-31 11:48:04 -0400

    Add batch function to collect the waveforms

---
+++ @CellSorter/batchFunction.m
@@ -0,0 +1,28 @@
+% batch function for use with RatCatcher
+% collects the mean spike waveforms from a root object
+% for a specified filename/filecode
+
+function batchFunction(index, batchname, location, outfile, test)
+
+  if ~test
+    addpath(genpath('/projectnb/hasselmogrp/hoyland/RatCatcher'))
+    addpath(genpath('/projectnb/hasselmogrp/hoyland/srinivas.gs_mtools'))
+    addpath(genpath('/projectnb/hasselmogrp/hoyland/CMBHOME'))
+  end
+
+  % acquire the filename and filecode
+  % the filecode should be the "cell number" as a 1x2 vector
+  [filename, filecode] = RatCatcher.read(location, batchname, index);
+
+  % load the data
+  % expect a 1x1 Session object named "root"
+  load(filename);
+
+  % acquire the waveform, which should be a 50x4 matrix in millivolts
+  % the first index is over time steps, the second over channels in the tetrode
+  waveform = [root.user_def.waveform(filecode(1), :).mean];
+
+  % save these data as a .csv file
+  csvwrite(outfile, waveform);
+
+end % function
