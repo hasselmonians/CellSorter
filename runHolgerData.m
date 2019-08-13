e82c4fb2abd642a179749c85c406093ac207aad9
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-13 14:33:15 -0400

    Add script to operate over Holger's data

---
+++ runHolgerData.m
@@ -0,0 +1,11 @@
+r = RatCatcher;
+
+r.expID = 'Holger';
+r.remotepath = '/projectnb/hasselmogrp/hoyland/CellSorter/cluster/';
+r.localpath = '/mnt/hasselmogrp/hoyland/CellSorter/cluster/';
+r.protocol = 'CellSorter';
+r.project = 'hasselmogrp';
+r.verbose = true;
+
+% batch files
+r.batchify;
