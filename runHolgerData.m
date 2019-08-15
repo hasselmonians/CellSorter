712ff1f2583706bf7efc927ed7ab3a0611ba9516
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-15 11:50:17 -0400

    Batch, gather, and cluster

--- runHolgerData.m
+++ runHolgerData.m
@@ -8,4 +8,35 @@ r.project = 'hasselmogrp';
 r.verbose = true;
 
 % batch files
-r.batchify;
+r = r.batchify;
+
+% NOTE: run the 'qsub' command on the cluster now (see output in MATLAB command prompt)
+
+return
+
+% NOTE: once the cluster finishes, run the following commands
+
+% gather files
+dataTable = r.gather;
+dataTable = r.stitch(dataTable);
+
+return
+
+% perform clustering
+
+X = NaN(height(dataTable), numel(dataTable.waveforms{1}));
+for ii = 1:height(dataTable)
+  X(ii, :) = dataTable.waveforms{ii}(:);
+end
+
+cs = CellSorter;
+cs.nClusters = 2;
+cs.nDims = 2;
+
+Y = cs.dimred(dataTable.waveforms);
+labels = cs.kcluster(Y);
+
+
+figure; hold on;
+scatter(Y(labels == 1), 1), Y(labels == 1, 2);
+scatter(Y(labels == 2), 1), Y(labels == 2, 2);
