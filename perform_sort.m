26dedadf70e0b55c632e591e3fedfe7e28d831bc
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-29 12:19:10 -0400

    Reorganize code, add comments

--- perform_sort.m
+++ perform_sort.m
@@ -1,5 +1,8 @@
 %% Performs the cell sorting
 
+
+%% Pre-processing
+
 % load the data from a local .mat file
 load('Holger-CellSorter.mat')
 
@@ -12,18 +15,30 @@ dataTable  = dataTable(~failing, :);
 
 % stack the waveforms and impute the data matrix
 % X = zeros(height(dataTable), numel(dataTable.waveforms{1}));
+channels = zeros(height(dataTable), 1);
 for ii = 1:height(dataTable)
   % X(ii, :) = corelib.vectorise(dataTable.waveforms{ii});
-  X(ii, :) = dataTable.waveforms{ii}(:, findStrongestChannel(dataTable.waveforms{ii}));
+  channels(ii) = findStrongestChannel(dataTable.waveforms{ii});
+  X(ii, :) = dataTable.waveforms{ii}(:, channels(ii));
 end
+
+%% Perform the cell sorting procedure
 
 % instantiate the CellSorter object
 cs = CellSorter;
 cs.algorithm = 'umap';
 
+%% Update the data table and visualize results
+
 % perform dimensionality reduction and clustering
 Y = cs.dimred(X);
 labels = cs.kcluster(Y);
+
+% add the strongest channel to the data table
+dataTable.channels = channels;
+
+% add the labels to the data table
+dataTable.labels = labels;
 
 % visualize the results
 figure; hold on
@@ -34,8 +49,6 @@ xlabel('dimension 1 (a.u.)')
 ylabel('dimension 2 (a.u.)')
 figlib.pretty()
 
-% add the labels to the data table
-dataTable.labels = labels;
-
+return
 % save the results by overwriting the existing .mat file
 save('Holger-CellSorter.mat', 'dataTable', 'r', 'failing')
