5b70662eb68af37ecd243f7b91503e2d818bbf12
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-29 13:19:55 -0400

    Plot the waveforms by cluster

--- perform_sort.m
+++ perform_sort.m
@@ -62,6 +62,24 @@ xlabel('dimension 1 (a.u.)')
 ylabel('dimension 2 (a.u.)')
 figlib.pretty()
 
+% plot the waveforms, grouped by cluster
+figure;
+time_points = (1/50) * 1:length(dataTable.waveforms{1}); % s
+for qq = 1:2
+  ax(qq) = subplot(1, 2, qq); hold on;
+  indices = find(dataTable.labels == qq);
+  for ii = 1:length(indices)
+    plot(time_points, dataTable.waveforms{indices(ii)}(:, dataTable.channels(indices(ii))));
+  end
+  xlabel('time (s)')
+  ylabel('voltage deflection (a.u.)')
+  title(['class ' num2str(qq) ' waveforms'])
+end
+
+linkaxes(ax, 'xy')
+figlib.pretty('LineWidth', 1, 'PlotBuffer', 0.1);
+
+
 return
 % save the results by overwriting the existing .mat file
 save('Holger-CellSorter.mat', 'dataTable', 'r', 'failing')
