bf919c0cafead888293ddcc17a8bb0f86165f13e
Alec Hoyland <alec.hoyland@posteo.net>
2019-09-06 14:54:34 -0400

    Correctly plot things

--- make_plots.m
+++ make_plots.m
@@ -1,4 +1,9 @@
-% plot the reduced data, colored by cluster
+%% Load the data
+
+load('data/Holger-CellSorter-processed.mat');
+
+%% Plot the reduced data, colored by cluster
+
 figure; hold on
 scatter(Y(dataTable.labels == 1, 1), Y(dataTable.labels == 1, 2))
 scatter(Y(dataTable.labels == 2, 1), Y(dataTable.labels == 2, 2))
@@ -7,15 +12,46 @@ xlabel('dimension 1 (a.u.)')
 ylabel('dimension 2 (a.u.)')
 figlib.pretty()
 
-% plot the waveforms, grouped by cluster
+%% Plot the waveforms, grouped by cluster
+
+% cell array containing matrices of variable size
+best_waveforms = cell(2,1);
+for qq = 1:2
+  % find linear indices to cells classified into qq
+  indices = find(dataTable.labels == qq);
+  % collect the best-channel waveforms into a large matrix
+  % the first dimension of the matrix is the number of cells classified into that class
+  % the second dimension is the length of the waveforms
+  best_waveforms{qq} = NaN(length(indices), size(dataTable.waveforms{1}, 1));
+  for ii = 1:length(indices)
+    best_waveforms{qq}(ii, :) = dataTable.waveforms{indices(ii)}(:, dataTable.channels(indices(ii)));
+  end
+end
+
 figure;
-time_points = (1/50) * 1:length(dataTable.waveforms{1}); % s
+time_points = 1:length(dataTable.waveforms{1}); % a.u.
 for qq = 1:2
   ax(qq) = subplot(1, 2, qq); hold on;
-  indices = find(dataTable.labels == qq);
-  for ii = 1:length(indices)
-    plot(time_points, dataTable.waveforms{indices(ii)}(:, dataTable.channels(indices(ii))));
+  cmap = colormaps.linspecer(size(best_waveforms{qq}, 1));
+  for ii = 1:size(best_waveforms{qq}, 1)
+    plot(ax(qq), time_points, best_waveforms{qq}(ii, :), 'Color', cmap(ii, :));
   end
+  xlabel('time (s)')
+  ylabel('voltage deflection (a.u.)')
+  title(['class ' num2str(qq) ' waveforms'])
+end
+
+linkaxes(ax, 'xy')
+figlib.pretty('LineWidth', 1, 'PlotBuffer', 0.1);
+
+%% Plot the waveforms, grouped by cluster
+% shaded by standard deviation
+
+figure;
+time_points = 1:length(dataTable.waveforms{1}); % a.u.
+for qq = 1:2
+  ax(qq) = subplot(1, 2, qq); hold on;
+  plotlib.errorShade(time_points, mean(best_waveforms{qq}), std(best_waveforms{qq}));
   xlabel('time (s)')
   ylabel('voltage deflection (a.u.)')
   title(['class ' num2str(qq) ' waveforms'])
