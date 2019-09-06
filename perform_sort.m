6bb39495ecce7ee004607113b75dc6700cb6047a
Alec Hoyland <alec.hoyland@posteo.net>
2019-09-06 12:09:43 -0400

    Move plotting to new script

--- perform_sort.m
+++ perform_sort.m
@@ -36,7 +36,7 @@ else
   end
 
   % add the firing rate as a feature
-  X = [X dataTable.firing_rate];
+  % X = [X dataTable.firing_rate];
 
   %% Perform the cell sorting procedure
 
@@ -59,32 +59,5 @@ else
   % save the results by overwriting the existing .mat file
   save('data/Holger-CellSorter-processed.mat', 'dataTable', 'r', 'Y')
 end
-
-% plot the reduced data, colored by cluster
-figure; hold on
-scatter(Y(dataTable.labels == 1, 1), Y(dataTable.labels == 1, 2))
-scatter(Y(dataTable.labels == 2, 1), Y(dataTable.labels == 2, 2))
-title('Holger-CellSorter UMAP dim-red / k-means clustering')
-xlabel('dimension 1 (a.u.)')
-ylabel('dimension 2 (a.u.)')
-figlib.pretty()
-
-% plot the waveforms, grouped by cluster
-figure;
-time_points = (1/50) * 1:length(dataTable.waveforms{1}); % s
-for qq = 1:2
-  ax(qq) = subplot(1, 2, qq); hold on;
-  indices = find(dataTable.labels == qq);
-  for ii = 1:length(indices)
-    plot(time_points, dataTable.waveforms{indices(ii)}(:, dataTable.channels(indices(ii))));
-  end
-  xlabel('time (s)')
-  ylabel('voltage deflection (a.u.)')
-  title(['class ' num2str(qq) ' waveforms'])
-end
-
-linkaxes(ax, 'xy')
-figlib.pretty('LineWidth', 1, 'PlotBuffer', 0.1);
-
 
 return
