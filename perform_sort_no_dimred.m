a3b216027032a9cd86ed5fb8b4536c7b5ceb594a
Alec Hoyland <alec.hoyland@posteo.net>
2019-10-02 14:46:22 -0400

    Rewrite script to look at spike width and firing rate

--- perform_sort_no_dimred.m
+++ perform_sort_no_dimred.m
@@ -32,50 +32,87 @@ else
     channels(ii) = findStrongestChannel(dataTable.waveforms{ii});
   end
 
+  % add the strongest channel to the data table
+  dataTable.channels = channels;
+
   %% Perform the cell sorting procedure
 
   % instantiate the CellSorter object
   cs = CellSorter;
-  cs.algorithm = 'umap';
 
   %% Update the data table and visualize results
 
   % perform dimensionality reduction and clustering
-  Y = [dataTable.spike_width];%, dataTable.firing_rate];
-  labels = cs.kcluster(Y);
+  labels_sw = cs.kcluster(dataTable.spike_width);
+  labels_fr = cs.kcluster(dataTable.firing_rate);
 
-  % add the strongest channel to the data table
-  dataTable.channels = channels;
+  % diagram of classification
+
+  %   firing_rate
+  %       ^
+  %       |
+  %  FSI  |  FSI
+  %       |
+  % --------------->  spike_width
+  %       |
+  %  FSI  |  PYR
+  %       |
+
+  % if a cell has a low firing rate and a large spike width, it is pyramidal
+  % check to see which label corresponds to large spike width
+  if mean(dataTable.spike_width(labels_sw == 1)) > mean(dataTable.spike_width(labels_sw == 2))
+    flag_sw = 1;
+  else
+    flag_sw = 2;
+  end
+
+  if mean(dataTable.firing_rate(labels_fr == 1)) > mean(dataTable.firing_rate(labels_fr == 2))
+    flag_fr = 1;
+  else
+    flag_fr = 2;
+  end
+
+  dataTable.isPyramidal = labels_sw == flag_sw & labels_fr ~= flag_fr;
 
   % add the labels to the data table
-  dataTable.labels = labels;
+  dataTable.labels_sw = labels_sw;
+  dataTable.labels_fr = labels_fr;
 
   % save the results by overwriting the existing .mat file
-  save('Holger-CellSorter-processed.mat', 'dataTable', 'r', 'Y')
+  save('Holger-CellSorter-processed.mat', 'dataTable', 'r')
 end
 
 % plot the reduced data, colored by cluster
 figure; hold on
-scatter(Y(dataTable.labels == 1, 1), Y(dataTable.labels == 1, 2))
-scatter(Y(dataTable.labels == 2, 1), Y(dataTable.labels == 2, 2))
-title('Holger-CellSorter UMAP dim-red / k-means clustering')
-xlabel('dimension 1 (a.u.)')
-ylabel('dimension 2 (a.u.)')
+scatter(dataTable.firing_rate(dataTable.isPyramidal), dataTable.spike_width(dataTable.isPyramidal));
+scatter(dataTable.firing_rate(~dataTable.isPyramidal), dataTable.spike_width(~dataTable.isPyramidal));
+title('Holger-CellSorter / k-means clustering')
+xlabel('firing rate (Hz)')
+ylabel('spike width (ms)')
+legend({'pyr', 'fsi'}, 'Location', 'best')
 figlib.pretty()
 
 % plot the waveforms, grouped by cluster
 figure;
 time_points = (1/50) * 1:length(dataTable.waveforms{1}); % s
-for qq = 1:2
-  ax(qq) = subplot(1, 2, qq); hold on;
-  indices = find(dataTable.labels == qq);
-  for ii = 1:length(indices)
-    plot(time_points, dataTable.waveforms{indices(ii)}(:, dataTable.channels(indices(ii))));
-  end
-  xlabel('time (s)')
-  ylabel('voltage deflection (a.u.)')
-  title(['class ' num2str(qq) ' waveforms'])
+
+ax(1) = subplot(1, 2, 1); hold on;
+indices = find(dataTable.isPyramidal);
+for ii = 1:length(indices)
+  plot(time_points, dataTable.waveforms{indices(ii)}(:, dataTable.channels(indices(ii))));
 end
+xlabel('time (s)')
+ylabel('voltage deflection (a.u.)')
+title(['pyramidal waveforms'])
+
+ax(2) = subplot(1, 2, 2); hold on;
+indices = find(~dataTable.isPyramidal);
+for ii = 1:length(indices)
+  plot(time_points, dataTable.waveforms{indices(ii)}(:, dataTable.channels(indices(ii))));
+end
+xlabel('time (s)')
+ylabel('voltage deflection (a.u.)')
+title(['fast-spiking interneuron waveforms'])
 
 linkaxes(ax, 'xy')
 figlib.pretty('LineWidth', 1, 'PlotBuffer', 0.1);
